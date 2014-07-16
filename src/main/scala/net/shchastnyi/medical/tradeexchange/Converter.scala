83bcefaa63a3f273dd4b82ae3de41671d0e86b94
Anton Schastny <schaan@ukr.net>
2014-07-16 19:34:28 +0400

    Launching stream: doc->docx->pdf

--- src/main/scala/net/shchastnyi/medical/tradeexchange/Converter.scala
+++ src/main/scala/net/shchastnyi/medical/tradeexchange/Converter.scala
@@ -2,14 +2,16 @@ package net.shchastnyi.medical.tradeexchange
 
 object Converter extends App {
 
-  val docConverter = new DocToDocx("d:/msconverter/", """d:\\tmp\\scala""", """d:\\tmp\\scala\\converted""")
+  val destinationDir = """d:\\tmp\\scala\\converted""";
+
+  val docConverter = new DocToDocx("d:/msconverter/", """d:\\tmp\\scala""", destinationDir)
   docConverter.setFoldersToConvert
   docConverter.setDestinationPathTemplate
   val step1Result = docConverter.convert
 
-//  if (step1Result == 0) {
-//    val doccer = new DocxToPdf
-//    doccer.convert("d:/tmp/test.docx")
-//  }
+  if (step1Result == 0) {
+    val doccer = new DocxToPdf
+    doccer.convert(destinationDir+"\\test.docx")
+  }
 
 }
