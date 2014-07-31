a700142fb27d921abfcb2489653d51b4dec04da8
Anton Schastny <schaan@ukr.net>
2014-07-31 13:55:12 +0400

    Refactoring + extracting params to utility options (+ default params)

--- src/main/scala/net/shchastnyi/medical/tradeexchange/Converter.scala
+++ src/main/scala/net/shchastnyi/medical/tradeexchange/Converter.scala
@@ -1,39 +1,94 @@
 package net.shchastnyi.medical.tradeexchange
 
 import java.io.{File, PrintWriter}
+import java.text.SimpleDateFormat
 import akka.actor.{ActorSystem, Props}
 import net.shchastnyi.actors.{ConvertDirectory, DirConverter}
 
 object Converter {
 
-  val ofcDir = "d:/msconverter/"
-  val sourceDir = """d:\\tmp\\scala"""
-  val destinationDir = """d:\\tmp\\scala\\converted"""
-  val translitPrefix = "2014_07_14_"
-  val urlPrefix = "http://1gb.sebastopol.ua/media/"
+  // Default options
+  val ofcDirDefault         = "d:/msconverter"
+  val sourceDirDefault      = "d:/tmp/scala"
+  val filePrefixDefault     = new SimpleDateFormat("yyyy_MM_dd_").format(new java.util.Date())
+  val urlPrefixDefault      = "http://1gb.sebastopol.ua/media/"
+  // !Default options
+
+  val ofcDirKey     = "--ofcDir"
+  val sourceDirKey  = "--sourceDir"
+  val destDirKey    = "--destDir"
+  val filePrefixKey = "--filePrefix"
+  val urlPrefixKey  = "--urlPrefix"
+  val usage = s"Usage: converter [$ofcDirKey ofcDir] [$sourceDirKey sourceFilesDir] [$destDirKey destinationDir] [$filePrefixKey String] [$urlPrefixKey url]"
+  type OptionMap = Map[String, String]
 
   def main (args: Array[String]) {
-    doTranslit
-    doDocx
-    doPdfLinear
-//    doPdfConcurrent
+    // Parsing command-line args
+    if (args.length == 0) println(usage)
+    val options         = getArgsAsMap(Map(), args.toList)
+    val ofcDir          = options.getOrElse(ofcDirKey, ofcDirDefault)
+    val sourceDir       = options.getOrElse(sourceDirKey, sourceDirDefault)
+    val destinationDir  = options.getOrElse(destDirKey, sourceDir + "/converted")
+    val filePrefix      = options.getOrElse(filePrefixKey, filePrefixDefault)
+    val urlPrefix       = options.getOrElse(urlPrefixKey, urlPrefixDefault)
+    // !Parsing command-line args
 
-    val html = doHtml
-    val s = new PrintWriter(sourceDir+"/tradingdocs.html")
-    s.print(html)
-    s.close()
+    // Doing main job
+    doTranslit(filePrefix, sourceDir)
+    doDocx(ofcDir, sourceDir, destinationDir)
+    doPdfLinear(destinationDir)
+    //doPdfConcurrent(destinationDir)
+    doHtml(destinationDir, urlPrefix)
+    // !Doing main job
   }
 
-  def doTranslit {
+  /**
+   * Parsing command-line args
+   * @param map
+   * @param list
+   * @return
+   */
+  private def getArgsAsMap(map: OptionMap, list: List[String]): OptionMap = {
+    def isSwitch(s : String) = s(0) == '-'
+    list match {
+      case Nil                                      => map
+      case ofcDirKey :: value :: tail               => getArgsAsMap(map ++ Map(ofcDirKey -> value), tail)
+      case sourceDirKey :: value :: tail            => getArgsAsMap(map ++ Map(sourceDirKey -> value), tail)
+      case destDirKey :: value :: tail              => getArgsAsMap(map ++ Map(destDirKey -> value), tail)
+      case filePrefixKey :: value :: tail           => getArgsAsMap(map ++ Map(filePrefixKey -> value), tail)
+      case urlPrefixKey :: value :: tail            => getArgsAsMap(map ++ Map(urlPrefixKey -> value), tail)
+      case string :: opt2 :: tail if isSwitch(opt2) => getArgsAsMap(map ++ Map("infile" -> string), list.tail)
+      case string :: Nil                            => getArgsAsMap(map ++ Map("infile" -> string), list.tail)
+      case option :: tail                           => println("Unknown option "+option); map
+    }
+  }
+
+  /**
+   * Translate from cyrillic to latin file names
+   * @param translitPrefix
+   * @param sourceDir
+   */
+  private def doTranslit(translitPrefix: String, sourceDir: String) {
     new File(sourceDir).listFiles().filter(_.getName.endsWith(".doc")).foreach {
       f => f.renameTo(new File(sourceDir+"/"+translitPrefix+Translit(f.getName)))
     }
   }
 
-  def doDocx = new DocToDocx(ofcDir, sourceDir, destinationDir).convert
+  /**
+   * Convert from doc to docx
+   * @param ofcDir
+   * @param sourceDir
+   * @param destinationDir
+   * @return
+   */
+  private def doDocx(ofcDir: String, sourceDir: String, destinationDir: String) =
+    new DocToDocx(ofcDir, sourceDir, destinationDir).convert
 
-
-  def doPdfLinear {
+  /**
+   * Convert from docx to pdf
+   * @param destinationDir
+   */
+  private def doPdfLinear(destinationDir: String) {
     val pdfConverter = new DocxToPdf
     val docxFiles = new File(destinationDir).listFiles().filter(_.getName.endsWith(".docx"))
     docxFiles foreach { f => pdfConverter.convert(f.getAbsolutePath) }
@@ -42,12 +97,22 @@ object Converter {
   /**
    * Warning: Not Safe!
    */
-  def doPdfConcurrent {
+  private def doPdfConcurrent(destinationDir: String) {
     val system = ActorSystem("akka-system")
     val converter = system.actorOf(Props[DirConverter], "mainConverter")
     converter ! ConvertDirectory(destinationDir)
   }
 
-  def doHtml = DocParser(destinationDir, urlPrefix)
+  /**
+   * Get titles from pdf documents, construct summary and write it into html file
+   * @param destinationDir
+   * @param urlPrefix
+   */
+  private def doHtml(destinationDir: String, urlPrefix: String) = {
+    val html = DocParser(destinationDir, urlPrefix)
+    val s = new PrintWriter(destinationDir+"/tradingdocs.html")
+    s.print(html)
+    s.close()
+  }
 
 }
--- src/main/scala/net/shchastnyi/medical/tradeexchange/DocToDocx.scala
+++ src/main/scala/net/shchastnyi/medical/tradeexchange/DocToDocx.scala
@@ -1,6 +1,7 @@
 package net.shchastnyi.medical.tradeexchange
 
 import java.io.{File, PrintWriter}
+import java.nio.file.{Files, Paths}
 
 import scala.io.Source
 import scala.sys.process._
@@ -8,12 +9,12 @@ import scala.sys.process._
 /**
  * Class for bulk converting doc files to docx<br/>
  * Uses <i>ofc.exe</i> behind the scenes.<br/>
- * Uses <i>ofc.ini</i> config file which is located at <b>ofcDir/Tools</b>.<br/>
+ * Uses <i>ofc.ini</i> config file which is located at <b>ofcDirDefault/Tools</b>.<br/>
  * To know more about OFC please refer to <a href="http://technet.microsoft.com/en-us/library/cc179019%28v=office.14%29.aspx">http://technet.microsoft.com/en-us/library/cc179019%28v=office.14%29.aspx</a><br/>
  * Use {@link #convert() convert} to trigger converting.
  * @param ofcDir A directory where The Office Migration Planning Manager (OMPM) has been installed
- * @param sourceFolder """d:\\tmp\\scala"""
- * @param destinationFolder """d:\\tmp\\scala\\converted"""
+ * @param sourceFolder "d:/tmp/scala"
+ * @param destinationFolder """d:/tmp/scala/converted"""
  */
 class DocToDocx(ofcDir: String, sourceFolder: String, destinationFolder: String) {
 
@@ -24,29 +25,43 @@ class DocToDocx(ofcDir: String, sourceFolder: String, destinationFolder: String)
   def convert: Int = {
     this.setFoldersToConvert
     this.setDestinationPathTemplate
-    val toolsDir = ofcDir + "Tools/"
+    val toolsDir = ofcDir + "/Tools/"
     val convertingCommand = toolsDir + "ofc.exe " + toolsDir + "ofc.ini"
     convertingCommand.!
+
+    // Moving files
+    val ofcResultDir = destinationFolder+sourceFolder.replaceFirst("d:","")
+    val destDir = Paths.get(destinationFolder)
+    new File(ofcResultDir).listFiles().filter(_.getName.endsWith(".docx")).foreach {
+      f => {
+        val source = Paths.get(f.getPath)
+        java.nio.file.Files.copy(source, destDir.resolve(source.getFileName()))
+        f.delete();
+      }
+    }
+    Files.delete(Paths.get(ofcResultDir))
+    // !Moving files
+    0
   }
 
   /**
-   * fldr=d:\tmp\scala //FoldersToConvert
+   * fldr=d:/tmp/scala //FoldersToConvert
    */
-  def setFoldersToConvert {
+  def setFoldersToConvert() {
     val func = (x: String) => x.replaceAll("(fldr=){1}(.)*", "fldr="+sourceFolder)
     replaceInFile(func)
   }
 
   /**
-   * DestinationPathTemplate=d:\tmp\scala\converted
+   * DestinationPathTemplate=d:/tmp/scala/converted
    */
-  def setDestinationPathTemplate {
+  def setDestinationPathTemplate() {
     val func = (x: String) => x.replaceAll("(DestinationPathTemplate=){1}(.)*", "DestinationPathTemplate="+destinationFolder)
     replaceInFile(func)
   }
 
   private def replaceInFile(func: (String) => (String)) = {
-    val iniString = ofcDir + "Tools/ofc.ini"
+    val iniString = ofcDir + "/Tools/ofc.ini"
 
     val source = Source.fromFile(iniString)
     val lines = source.mkString.split("\\n").toList.map(func)
@@ -54,7 +69,7 @@ class DocToDocx(ofcDir: String, sourceFolder: String, destinationFolder: String)
 
     val writer = new PrintWriter(iniString)
     for(line <- lines)
-      writer.print(line)
+      writer.print(line.replace("/","\\"))
     writer.close()
   }
 
